490fc908ff5dc52d2775faebaff9eeef2b791d18
H2Owater425 <h2o@h2owr.xyz>
2023-05-18 17:48:48 +0900

    fix: Fix prisma client parameter

--- source/library/database.ts
+++ source/library/database.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { Redis } from 'ioredis';
 
-export const prisma: PrismaClient = new PrismaClient(process['env']['DATABASE_URL']);
+export const prisma: PrismaClient = new PrismaClient();
 
 export const redis: Redis = new Redis(process['env']['CACHE_DATABASE_URL']);
