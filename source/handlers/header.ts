aa89786dab84036aff1d9096820b098d32b702ca
H2Owater425 <h2o@h2owr.xyz>
2023-06-21 13:14:37 +0900

    feat: Fix cors related header

--- source/handlers/header.ts
+++ source/handlers/header.ts
@@ -3,10 +3,8 @@ import { DoneFuncWithErrOrRes, FastifyRequest, FastifyReply } from 'fastify';
 export default function headerHandler(request: FastifyRequest, reply: FastifyReply, done: DoneFuncWithErrOrRes): void {
 	reply.headers({
 		'Content-Security-Policy': "default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
-		'Cross-Origin-Embedder-Policy': 'require-corp',
 		'Cross-Origin-Opener-Policy': 'same-origin',
 		'Cross-Origin-Resource-Policy': 'same-origin',
-		'Expect-CT': 'max-age=0',
 		'Origin-Agent-Cluster': '?1',
 		'Referrer-Policy': 'no-referrer',
 		'Strict-Transport-Security': 'max-age=15552000; includeSubDomains',
@@ -18,6 +16,7 @@ export default function headerHandler(request: FastifyRequest, reply: FastifyRep
 		'X-XSS-Protection': '0',
 		'Access-Control-Allow-Methods': '*',
 		'Access-Control-Allow-Headers': '*',
+		"Access-Control-Request-Method": '*'
 	});
 
 	done();
