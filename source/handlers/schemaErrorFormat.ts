992a28c3737688b64f94dcfea10130005de48fff
H2Owater425 <h2o@h2owr.xyz>
2023-06-21 13:14:56 +0900

    feat: Fix instance path style

--- source/handlers/schemaErrorFormat.ts
+++ source/handlers/schemaErrorFormat.ts
@@ -1,5 +1,15 @@
 import { FastifySchemaValidationError } from 'fastify/types/schema';
 
 export default function schemaErrorFormatHandler(errors: FastifySchemaValidationError[], dataVariableName: string): Error {
-	return new Error(dataVariableName + ' ' + (errors[0]['instancePath']['length'] !== 0 ? errors[0]['instancePath'].slice(1) + ' ' : '') + errors[0]['message']);
+	const instancePaths: string[] = errors[0]['instancePath'].slice(1).split('.');
+
+	dataVariableName = String.fromCharCode(dataVariableName.charCodeAt(0) - 32) + dataVariableName.slice(1);
+
+	if(instancePaths[0] !== '') {
+		for(let i: number = 0; i < instancePaths['length']; i++) {
+			dataVariableName += '[\'' + instancePaths[i].replace(/'/g, '\\\'') + '\']';
+		}
+	}
+
+	return new Error(typeof(errors[0]['message']) === 'string' ? dataVariableName + ' ' + errors[0]['message'].replace(/"/g, '\'') : 'Unknown');
 }
