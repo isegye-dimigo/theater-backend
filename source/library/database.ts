948b00581197a5e01f102093a20addc6c98ecaf6
H2Owater425 <h2o@h2owr.xyz>
2023-09-09 01:06:32 +0900

    feat: Add redis

--- source/library/database.ts
+++ source/library/database.ts
@@ -3,20 +3,4 @@ import { Redis } from 'ioredis';
 
 export const prisma: PrismaClient = new PrismaClient();
 
-export const redis: Redis = new Redis(process['env']['CACHE_DATABASE_URL']);
-
-export function getPagenation(query: PageQuery): {
-	skip: number,
-	take: number,
-	orderBy: {
-		id: 'asc' | 'desc'
-	}
-} {
-	return {
-		skip: query['page[size]'] * query['page[index]'],
-		take: query['page[size]'],
-		orderBy: {
-			id: query['page[order]'] === 'asc' ? 'asc' : 'desc'
-		}
-	};
-}
+export const redis: Redis = new Redis(process['env']['SUB_DATABASE_URL']);
