85faeb4d4fb0fc9c92e3488646204f1813e55454
H2Owater425 <h2o@h2owr.xyz>
2023-09-09 01:04:02 +0900

    feat: Add PageQuery and ffprobe related types

--- source/library/type.ts
+++ source/library/type.ts
@@ -13,7 +13,7 @@ export interface RouteOptions extends Omit<_RouteOptions, 'handler' | 'schema'> 
 	method: HTTPMethods;
 	handler: RouteHandlerMethod<Server, IncomingMessage, ServerResponse, any, any, FastifySchema, FastifyTypeProvider, FastifyBaseLogger>;
 	schema?: Partial<Pick<RecursiveRecord<string, | Partial<Record<'$isRequired', boolean>> | Omit<Record<string, JSONSchema>, '$isRequired'>>, SchemaKey>>;
-	excludePreHandler?: boolean;
+	isAuthNeeded?: boolean;
 }
 
 export interface ModuleOptions {
@@ -39,3 +39,57 @@ export type JsendResponse = {
 export type ResolveFunction<T = void> = (value: T) => void;
 
 export type RejectFunction = (error: any) => void;
+
+export interface PageQuery {
+	'page[size]': number;
+	'page[index]': number;
+	'page[order]': 'desc' | 'asc';
+}
+
+export interface FileSignature {
+	offsetByte: number;
+	buffer: Buffer;
+}
+
+export type FileType = 'mp4' | 'jpg' | 'png';
+
+export interface Metadata {
+	video: {
+		width: number;
+		height: number;
+		frameRate: number;
+		aspectRatio: string;
+		bitRate: number;
+	};
+
+	audio: {
+		channelCount: number;
+		sampleRate: number;
+		bitRate: number;
+	};
+
+	duration: number;
+	size: number;
+	bitRate: number;
+}
+
+export interface VideoStream {
+	index: number;
+	codec_type: 'video';
+	width: number;
+	height: number;
+	avg_frame_rate: string;
+	bit_rate: string;
+}
+
+export interface AudioStream {
+	index: number;
+	codec_type: 'audio';
+	sample_rate: string;
+	channels: number;
+	bit_rate: string;
+}
+
+export type NonNullableRequired<T> = {
+  [P in keyof T]-?: NonNullable<T[P]>;
+};
