9dfe18c4b4f089faa4a8851e7f007ed6aeb9a7ec
H2Owater425 <h2o@h2owr.xyz>
2023-09-09 01:09:52 +0900

    feat: Implement movieLike module

---
+++ source/routes/movies/movieLike/deleteMovieLike.controller.ts
@@ -0,0 +1,56 @@
+import { prisma } from '@library/database';
+import { NotFound } from '@library/httpError';
+import { Movie, Prisma } from '@prisma/client';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export default function (request: FastifyRequest<{
+	Params: {
+		movieId: Movie['id'];
+	}
+}>, reply: FastifyReply): void {
+	prisma['movie'].count({
+		where: {
+			OR: [{
+				id: request['params']['movieId'],
+				isDeleted: false
+			}, {
+				id: request['params']['movieId'],
+				isDeleted: false,
+				movieLikes: {
+					some: {
+						movieId: request['params']['movieId'],
+						userId: request['user']['id']
+					}
+				}
+			}]
+		}
+	})
+	.then(function (movieCount: number): Promise<Prisma.BatchPayload> {
+		switch(movieCount) {
+			default: {
+				return prisma['movieLike'].deleteMany({
+					where: {
+						id: request['params']['movieId'],
+						userId: request['user']['id']
+					}
+				});
+			}
+			case 1: {
+				throw new NotFound('User must liked first');
+			}
+			case 0: {
+				throw new NotFound('Parameter[\'movieId\'] must be valid');
+			}
+		}
+	})
+	.then(function (result: Prisma.BatchPayload): void {
+		if(result['count'] === 1) {
+			reply.status(204).send(null);
+		} else {
+			throw new NotFound('Parameter[\'movieId\'] must be valid');
+		}
+	})
+	.catch(reply.send.bind(reply));
+	
+	return;
+}
---
+++ source/routes/movies/movieLike/movieLike.module.ts
@@ -0,0 +1,30 @@
+import Module from '@library/module';
+import postMovieLikeController from './postMovieLike.controller';
+import movieSchema from '@schemas/movie';
+import deleteMovieLikeController from './deleteMovieLike.controller';
+
+export default new Module({
+	routers: [{
+		method: 'POST',
+		url: '',
+		handler: postMovieLikeController,
+		schema: {
+			params: {
+				movieId: movieSchema.get('id').required()
+			}
+		},
+		isAuthNeeded: true
+	}, {
+		method: 'DELETE',
+		url: '',
+		handler: deleteMovieLikeController,
+		schema: {
+			params: {
+				movieId: movieSchema.get('id').required()
+			}
+		},
+		isAuthNeeded: true
+	}],
+	prefix: ':movieId/like',
+	modules: []
+});
