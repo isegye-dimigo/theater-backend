7a935604b2c763977f7176114d7ff7dba301fd9f
H2Owater425 <h2o@h2owr.xyz>
2023-09-09 01:07:51 +0900

    feat: Add LOG_LEVEL to environment variable type

--- source/types/global.d.ts
+++ source/types/global.d.ts
@@ -1,9 +1,11 @@
 import { REQUIRED_ENVIRONMENT_VARIABLE_NAMES } from '@library/environment';
+import { LogLevel } from 'fastify';
 
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv extends Record<typeof REQUIRED_ENVIRONMENT_VARIABLE_NAMES[number], string> {
 			NODE_ENV: 'development' | 'production';
+			LOG_LEVEL: LogLevel;
 			JSON_WEB_TOKEN_SECRET: string;
 		}
 	}
