8d54cc99368432aa49c75fa4e8484920dafce5f2
H2Owater425 <h2o@h2owr.xyz>
2023-09-08 23:01:28 +0900

    feat: Add environment variables

--- source/library/environment.ts
+++ source/library/environment.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { randomBytes } from 'crypto';
 
-export const REQUIRED_ENVIRONMENT_VARIABLE_NAMES = ['DATABASE_URL', 'CACHE_DATABASE_URL', 'EMAIL_USER', 'EMAIL_PASSWORD', 'EMAIL_HOST', 'EMAIL_PORT', 'PORT', 'PBKDF2_ITERATION'] as const;
+export const REQUIRED_ENVIRONMENT_VARIABLE_NAMES = ['DATABASE_URL', 'SUB_DATABASE_URL', 'EMAIL_USER', 'EMAIL_PASSWORD', 'EMAIL_HOST', 'EMAIL_PORT', 'PORT', 'PBKDF2_ITERATION', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_BUCKET_NAME', 'AWS_CLOUDFRONT_URL'] as const;
 
 for(let i: number = 0; i < REQUIRED_ENVIRONMENT_VARIABLE_NAMES['length']; i++) {
 	if(typeof(process['env'][REQUIRED_ENVIRONMENT_VARIABLE_NAMES[i]]) === 'undefined') {
@@ -9,8 +9,26 @@ for(let i: number = 0; i < REQUIRED_ENVIRONMENT_VARIABLE_NAMES['length']; i++) {
 	}
 }
 
+process['env']['LOG_LEVEL'] ||= 'silent';
+
+switch(process['env']['LOG_LEVEL']) {
+	case 'fatal':
+	case 'error':
+	case 'warn':
+	case 'info':
+	case 'debug':
+	case 'trace':
+	case 'silent': {
+		break;
+	}
+
+	default: {
+		throw new Error('LOG_LEVEL must be valid');
+	}
+}
+
 process['env']['PORT'] ||= '80';
 
 process['env']['TZ'] = 'UTC';
 
-process['env']['JSON_WEB_TOKEN_SECRET'] = randomBytes(64).toString('hex');
+process['env']['JSON_WEB_TOKEN_SECRET'] = process['env']['NODE_ENV'] === 'production' ? randomBytes(64).toString('hex') : '김지훈빡빡이';
