b0e7412314166c9aaa6a3053932880529cefa7d1
H2Owater425 <h2o@h2owr.xyz>
2023-09-09 01:52:01 +0900

    feat: Add multipart plugin to fastify

--- source/application.ts
+++ source/application.ts
@@ -1,4 +1,5 @@
 import '@library/environment';
+import '@library/schedule';
 import Logger from '@library/logger';
 import fastify, { FastifyInstance } from 'fastify';
 import errorHandler from './handlers/error';
@@ -6,6 +7,8 @@ import headerHandler from './handlers/header';
 import notFoundHandler from './handlers/notFound';
 import serializeHandler from './handlers/serialize';
 import rootModule from './routes/root.module';
+import fastifyMultipart from '@fastify/multipart';
+import JsonWebToken from '@library/jsonWebToken';
 
 const fastifyInstance: FastifyInstance = fastify({
 	trustProxy: true,
@@ -21,6 +24,14 @@ fastifyInstance.setErrorHandler(errorHandler);
 fastifyInstance.setReplySerializer(serializeHandler);
 fastifyInstance.addHook('preHandler', headerHandler);
 
+fastifyInstance.register(fastifyMultipart, {
+	throwFileSizeLimit: true,
+	limits: {
+		files: 1,
+		fileSize: 2147000000
+	}
+});
+
 rootModule.register(fastifyInstance);
 
 fastifyInstance.listen({
@@ -30,13 +41,22 @@ fastifyInstance.listen({
 .then(function (): void {
 	fastifyInstance['log'].info('Route tree:');
 
-	const routeLines: string[] = fastifyInstance.printRoutes({ commonPrefix: false }).split(/^(└──\s|\s{4})/gm).slice(2);
+	const routeLines: string[] = fastifyInstance.printRoutes({
+		commonPrefix: false
+	}).split(/^(└──\s|\s{4})/gm).slice(2);
 
 	for(let i: number = 0; i < routeLines['length']; i++) {
 		if(i % 2 === 0) {
 			fastifyInstance['log'].info(routeLines[i].replace('\n', ''));
 		}
 	}
+
+	fastifyInstance['log'].debug('accessToken: ' + JsonWebToken.create({
+		uid: 0,
+		hdl: '',
+		vrf: true,
+		exp: Number['MAX_SAFE_INTEGER']
+	}, process['env']['JSON_WEB_TOKEN_SECRET']));
 
 	return;
 })
