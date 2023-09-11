0523b158128de2384ab5d63c327333ae3483c16e
H2Owater425 <h2o@h2owr.xyz>
2023-09-11 21:18:45 +0900

    fix: Fix json web token regular expression

--- source/schemas/common.ts
+++ source/schemas/common.ts
@@ -5,7 +5,7 @@ const commonSchema: Schema<'positiveInteger' | 'sha512Hex' | 'datetime' | 'jsonW
 	positiveInteger: schema.integer().minimum(1).maximum(Number['MAX_VALUE']),
 	sha512Hex: schema.string().pattern(/^[0-9a-f]{128}$/),
 	datetime: schema.string().format('date-time'),
-	jsonWebToken: schema.string().pattern(/^[a-z0-9-_]*\.[a-z0-9-_]*\.[a-z0-9-_]*$/)
+	jsonWebToken: schema.string().pattern(/^[A-Za-z0-9_-]{2,}(\.[A-Za-z0-9-_]{2,}){2}$/)
 });
 
 export default commonSchema;
