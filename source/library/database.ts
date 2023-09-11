a8edc0e45dcd43382aba9f452d020aa0c44bfb7d
H2Owater425 <h2o@h2owr.xyz>
2023-09-11 21:15:44 +0900

    feat: Add error handler for redis

--- source/library/database.ts
+++ source/library/database.ts
@@ -1,6 +1,28 @@
 import { PrismaClient } from '@prisma/client';
 import { Redis } from 'ioredis';
+import { logger } from './logger';
 
 export const prisma: PrismaClient = new PrismaClient();
 
 export const redis: Redis = new Redis(process['env']['SUB_DATABASE_URL']);
+
+redis.on('error', function (error: Error): void {
+	// @ts-expect-error
+	switch(error['code']) {
+		case 'ECONNRESET':
+		case 'ECONNREFUSED': {
+			// @ts-expect-error
+			logger.trace('redis ' + error['code']);
+
+			break;
+		}
+
+		default: {
+			logger.error(error);
+
+			return;
+		}
+	}
+	
+	return;
+});
