bff2047bbf28ea6b9606f2210c6d8d326a0ccbbb
H2Owater425 <h2o@h2owr.xyz>
2023-09-11 21:19:46 +0900

    feat: Simplify job into many jobs

--- source/library/schedule.ts
+++ source/library/schedule.ts
@@ -1,61 +1,100 @@
 import { MovieStatistic } from '@prisma/client';
 import { prisma, redis } from '@library/database';
-import Logger from '@library/logger';
-
-const logger: Logger = new Logger();
+import { logger } from '@library/logger';
+import { RejectFunction, ResolveFunction } from './type';
 
 setInterval(function (): void {
-	logger.debug('schedule started');
-	const movieViewKeys: string[] = [];
-	const movieStatisticIds: MovieStatistic['id'][] = [];
-	let transaction: string | undefined;
-
-	prisma.$queryRawUnsafe<Record<'f0' | 'f1', BigInt>[]>('INSERT INTO movie_statistic (movie_id, view_count, like_count, comment_count, star_average) SELECT movie_statistic.movie_id, movie_statistic.view_count, COUNT(movie_like.id), COUNT(movie_comment.id), COALESCE(AVG(movie_star.value), 0) FROM movie_statistic LEFT JOIN movie_like ON movie_statistic.movie_id = movie_like.movie_id LEFT JOIN movie_comment ON movie_statistic.movie_id = movie_comment.movie_id LEFT JOIN movie_star ON movie_statistic.movie_id = movie_star.movie_id INNER JOIN (SELECT MAX(id) AS id FROM movie_statistic GROUP BY movie_id) AS _movie_statistic ON movie_statistic.id = _movie_statistic.id WHERE movie_statistic.created_at <= NOW() - INTERVAL 10 MINUTE GROUP BY movie_statistic.movie_id RETURNING movie_statistic.id, movie_statistic.movie_id')
-	.then(function (results: Record<'f0' | 'f1', BigInt>[]): Promise<(string | null)[]> | undefined {
-		for(let i: number = 0; i < results['length']; i++) {
-			movieStatisticIds.push(Number(results[i]['f0']));
-
-			movieViewKeys.push('movieView:' + results[i]['f1'].toString(10));
+	prisma.$executeRawUnsafe('INSERT INTO movie_statistic (movie_id, view_count, like_count, comment_count, star_average) SELECT movie_id, view_count, like_count, comment_count, star_average FROM current_movie_statistic WHERE created_at <= NOW() - INTERVAL 10 MINUTE')
+	//prisma.$executeRawUnsafe('INSERT INTO movie_statistic (movie_id, view_count, like_count, comment_count, star_average) SELECT movie_id, view_count, like_count, comment_count, star_average FROM current_movie_statistic')
+	.then(function (affactedRowCount: number): void {
+		if(affactedRowCount !== 0) {
+			logger.debug(affactedRowCount + ' rows have been created');
 		}
 
-		if(movieViewKeys['length'] !== 0) {
-			return redis.mget(movieViewKeys);
+		return;
+	})
+	.catch(logger.error);
+
+	return;
+}, 60000);
+
+setInterval(function (): void {
+	prisma.$executeRawUnsafe('UPDATE movie_statistic, current_movie_statistic SET movie_statistic.comment_count = current_movie_statistic.comment_count, movie_statistic.like_count = current_movie_statistic.like_count, movie_statistic.star_average = current_movie_statistic.star_average WHERE movie_statistic.id = current_movie_statistic.id')
+	.then(function (affactedRowCount: number): void {
+		logger.debug(affactedRowCount + ' rows have been updated');
+
+		return;
+	})
+	.catch(logger.error);
+
+	return;
+}, 60000);
+
+setInterval(function (): void {
+	const movieIds: MovieStatistic['movieId'][] = [];
+	const keys: string[] = [];
+
+	new Promise<Set<string>>(function (resolve: ResolveFunction<Set<string>>, reject: RejectFunction): void {
+		const keys: Set<string> = new Set<string>();
+		let cursor: string = '0';
+		
+		(function scanKeys(): Promise<void> {
+			return redis.scan(cursor, 'MATCH', 'movieView:*')
+			.then(function (results: [string, string[]]): Promise<void> | void {
+				for(let i: number = 0; i < results[1]['length']; i++) {
+					keys.add(results[1][i]);
+				}
+
+				if(results[0] !== '0') {
+					cursor = results[0];
+
+					return scanKeys();
+				} else {
+					resolve(keys);
+
+					return;
+				}
+			})
+			.catch(reject);
+		})();
+
+		return;
+	})
+	.then(function (_keys: Set<string>): Promise<(string | null)[]> | [] {
+		if(_keys['size'] !== 0) {
+			for(const key of _keys) {
+				movieIds.push(Number.parseInt(key.slice(10), 10));
+				keys.push(key);
+			}
+	
+			return redis.mget(keys);
 		} else {
-			return;
+			return [];
 		}
 	})
-	.then(function (results: (string | null)[] | undefined): Promise<number> | undefined {
-		if(Array.isArray(results)) {
-			let queryCount: number = 0;
-			transaction = 'BEGIN NOT ATOMIC DECLARE EXIT HANDLER FOR SQLEXCEPTION, SQLWARNING BEGIN ROLLBACK;END;START TRANSACTION;';
-
+	.then(function (results: (string | null)[]): Promise<[number, number]> | [number, number] {
+		if(results['length'] !== 0) {
+			let transaction = 'BEGIN NOT ATOMIC DECLARE EXIT HANDLER FOR SQLEXCEPTION, SQLWARNING BEGIN ROLLBACK;END;START TRANSACTION;';
+	
 			for(let i: number = 0; i < results['length']; i++) {
-				if(typeof(results[i]) === 'string') {
-					transaction += 'UPDATE movie_statistic SET view_count = view_count + ' + results[i] + ' WHERE id = ' + movieStatisticIds[i] + ';';
-					queryCount++;
+				if(results[i] !== null) {
+					transaction += 'UPDATE movie_statistic, current_movie_statistic SET movie_statistic.view_count = movie_statistic.view_count + ' + results[i] + ' WHERE movie_statistic.id = current_movie_statistic.id AND movie_statistic.movie_id = ' + movieIds[i] + ';';
 				}
 			}
 	
-			if(queryCount !== 0) {
-				transaction += 'COMMIT;END';
-			} else {
-				transaction = undefined;
+			if(transaction['length'] !== 104) {
+				return Promise.all([redis.unlink(keys), prisma.$executeRawUnsafe(transaction + 'COMMIT;END')]);
 			}
-	
-			return redis.del(movieViewKeys);
-		} else {
-			return;
 		}
+
+		return [0, 0];
 	})
-	.then(function (): Promise<number> | number {
-		if(typeof(transaction) === 'string') {
-			return prisma.$executeRawUnsafe(transaction);
-		} else {
-			return 0;
+	.then(function (results: [number, number]): void {
+		if(results[0] !== 0 || results[1] !== 0) {
+			logger.debug(results[0] + ' views have been unlinked and, ' + results[1] + ' rows have been updated');
 		}
-	})
-	.then(function (affectedRowCount: number): void {
-		logger.debug(affectedRowCount + ' rows has affacted');
+
+		return;
 	})
 	.catch(logger.error);
 
