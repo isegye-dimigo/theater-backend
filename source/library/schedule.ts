a47c256c105c676bf84a00b8b6ce000a901b5bb3
H2Owater425 <h2o@h2owr.xyz>
2023-09-11 22:30:49 +0900

    feat: Use ON DUPLICATE to update many at a time

--- source/library/schedule.ts
+++ source/library/schedule.ts
@@ -74,16 +74,16 @@ setInterval(function (): void {
 	})
 	.then(function (results: (string | null)[]): Promise<[number, number]> | [number, number] {
 		if(results['length'] !== 0) {
-			let query = '';
+			const values: string[] = [];
 	
 			for(let i: number = 0; i < results['length']; i++) {
 				if(results[i] !== null) {
-					query += 'UPDATE movie_statistic, current_movie_statistic SET movie_statistic.view_count = movie_statistic.view_count + ' + results[i] + ' WHERE movie_statistic.id = current_movie_statistic.id AND movie_statistic.movie_id = ' + movieIds[i] + ';';
+					values.push('((SELECT id FROM current_movie_statistic WHERE movie_id = ' + movieIds[i] + '), ' + results[i] + ')');
 				}
 			}
 	
-			if(query['length'] !== 0) {
-				return Promise.all([redis.unlink(keys), prisma.$executeRawUnsafe(query)]);
+			if(values['length'] !== 0) {
+				return Promise.all([redis.unlink(keys), prisma.$executeRawUnsafe('INSERT IGNORE INTO movie_statistic (id, view_count) VALUES ' + values.join(', ') + ' ON DUPLICATE KEY UPDATE view_count = view_count + VALUES(view_count)')]);
 			}
 		}
 
@@ -91,7 +91,7 @@ setInterval(function (): void {
 	})
 	.then(function (results: [number, number]): void {
 		if(results[0] !== 0 || results[1] !== 0) {
-			logger.debug(results[0] + ' views have been unlinked and, ' + results[1] + ' rows have been updated');
+			logger.debug(results[0] + ' views have been unlinked and, ' + results[1] / 2 + ' rows have been updated');
 		}
 
 		return;
