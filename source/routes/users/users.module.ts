09ea368197e92b35d112847d8c287e192326f3a1
H2Owater425 <h2o@h2owr.xyz>
2023-09-18 00:16:16 +0900

    feat: Add user patch route

--- source/routes/users/users.module.ts
+++ source/routes/users/users.module.ts
@@ -1,6 +1,7 @@
 import Module from '@library/module';
 import userSchema from '@schemas/user';
 import postUsersController from './postUsers.controller';
+import patchUserController from './patchUser.controller';
 import deleteUserController from './deleteUser.controller';
 import getUserController from './getUser.controller';
 import userCommentsModule from './userComments/userComments.module';
@@ -16,6 +17,25 @@ export default new Module({
 				email: userSchema.get('email').required(),
 				password: userSchema.get('password').required(),
 				name: userSchema.get('name').required()
+			}
+		}
+	}, {
+		method: 'PATCH',
+		url: ':userHandle',
+		handler: patchUserController,
+		schema: {
+			params: {
+				userHandle: userSchema.get('handle').required()
+			},
+			body: {
+				email: userSchema.get('email'),
+				password: userSchema.get('password'),
+				handle: userSchema.get('handle'),
+				name: userSchema.get('name'),
+				description: userSchema.get('description'),
+				profileMediaId: userSchema.get('profileMediaId'),
+				bannerMediaId: userSchema.get('bannerMediaId'),
+				currentPassword: userSchema.get('password').required()
 			}
 		}
 	}, {
