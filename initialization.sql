161ce3302645e6e90734ab9ebf414fafed493c8a
H2Owater425 <h2o@h2owr.xyz>
2023-09-18 01:28:33 +0900

    feat: Apply changes to raw sql

--- initialization.sql
+++ initialization.sql
@@ -21,8 +21,9 @@ CREATE TABLE `media` (
   `is_deleted` bit(1) NOT NULL DEFAULT b'0',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `hash` (`hash`),
-  KEY `media.user_id,user.id` (`user_id`)
+  UNIQUE KEY `UNIQUE INDEX(hash)` (`hash`) USING BTREE,
+  KEY `FOREIGN KEY(media.user_id,user.id)` (`user_id`),
+  CONSTRAINT `FOREIGN KEY(media.user_id,user.id)` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `media_video` (
@@ -34,8 +35,8 @@ CREATE TABLE `media_video` (
   `video_bit_rate` int(10) unsigned NOT NULL,
   `audio_bit_rate` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `media_id, index` (`media_id`,`index`) USING BTREE,
-  CONSTRAINT `media_video.media_id,media.id` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+  UNIQUE KEY `UNIQUE INDEX(media_id,index)` (`media_id`,`index`) USING BTREE,
+  CONSTRAINT `FOREIGN KEY(media_video.media_id,media.id)` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `media_video_metadata` (
@@ -48,7 +49,7 @@ CREATE TABLE `media_video_metadata` (
   `channel_count` tinyint(2) unsigned NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `UNIQUE INDEX(media_id)` (`media_id`) USING BTREE,
-  CONSTRAINT `media_video_metadata.media_id,media.id` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `FOREIGN KEY(media_video_metadata.media_id,media.id)` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `movie` (
@@ -61,12 +62,12 @@ CREATE TABLE `movie` (
   `is_deleted` bit(1) NOT NULL DEFAULT b'0',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `video_id` (`video_media_id`),
-  UNIQUE KEY `image_id` (`image_media_id`),
-  KEY `movie.user_id,user.id` (`user_id`),
-  CONSTRAINT `movie.image_media_id,media.id` FOREIGN KEY (`image_media_id`) REFERENCES `media` (`id`),
-  CONSTRAINT `movie.user_id,user.id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `movie.video_media_id,media.id` FOREIGN KEY (`video_media_id`) REFERENCES `media` (`id`)
+  UNIQUE KEY `UNIQUE INDEX(video_media_id)` (`video_media_id`) USING BTREE,
+  UNIQUE KEY `UNIQUE INDEX(image_media_id)` (`image_media_id`) USING BTREE,
+  KEY `FOREIGN KEY(movie.user_id,user.id)` (`user_id`) USING BTREE,
+  CONSTRAINT `FOREIGN KEY(movie.image_media_id,media.id)` FOREIGN KEY (`image_media_id`) REFERENCES `media` (`id`),
+  CONSTRAINT `FOREIGN KEY(movie.user_id,user.id)` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FOREIGN KEY(movie.video_media_id,media.id)` FOREIGN KEY (`video_media_id`) REFERENCES `media` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `movie_comment` (
@@ -78,10 +79,10 @@ CREATE TABLE `movie_comment` (
   `is_deleted` bit(1) NOT NULL DEFAULT b'0',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `movie_comment.movie_id,movie.id` (`movie_id`),
-  KEY `movie_comment.user_id,user.id` (`user_id`),
-  CONSTRAINT `movie_comment.movie_id,movie.id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
-  CONSTRAINT `movie_comment.user_id,user.id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  KEY `FOREIGN KEY(movie_comment.movie_id,movie.id)` (`movie_id`) USING BTREE,
+  KEY `FOREIGN KEY(movie_comment.user_id,user.id)` (`user_id`) USING BTREE,
+  CONSTRAINT `FOREIGN KEY(movie_comment.movie_id,movie.id)` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
+  CONSTRAINT `FOREIGN KEY(movie_comment.user_id,user.id)` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `movie_like` (
@@ -90,10 +91,10 @@ CREATE TABLE `movie_like` (
   `user_id` int(10) unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `movie_id,user_id` (`movie_id`,`user_id`),
-  KEY `movie_like.user_id,user.id` (`user_id`),
-  CONSTRAINT `movie_like.movie_id,movie.id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
-  CONSTRAINT `movie_like.user_id,user.id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  UNIQUE KEY `UNIQUE INDEX(movie_id,user_id)` (`movie_id`,`user_id`) USING BTREE,
+  KEY `FOREIGN KEY(movie_like.user_id,user.id)` (`user_id`) USING BTREE,
+  CONSTRAINT `FOREIGN KEY(movie_like.movie_id,movie.id)` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
+  CONSTRAINT `FOREIGN KEY(movie_like.user_id,user.id)` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `movie_star` (
@@ -104,9 +105,9 @@ CREATE TABLE `movie_star` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQUE INDEX (user_id,movie_id)` (`user_id`,`movie_id`),
-  KEY `movie_star.movie_id,movie.id` (`movie_id`),
-  CONSTRAINT `movie_star.movie_id,movie.id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `movie_star.user_id,user.id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `FOREIGN KEY(movie_star.movie_id,movie.id)` (`movie_id`),
+  CONSTRAINT `FOREIGN KEY(movie_star.movie_id,movie.id)` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FOREIGN KEY(movie_star.user_id,user.id)` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `movie_statistic` (
@@ -118,8 +119,8 @@ CREATE TABLE `movie_statistic` (
   `star_average` double unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `movie_statistic.movie_id,movie.id` (`movie_id`),
-  CONSTRAINT `movie_statistic.movie_id,movie.id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `FOREIGN KEY(movie_statistic.movie_id,movie.id)` (`movie_id`),
+  CONSTRAINT `FOREIGN KEY(movie_statistic.movie_id,movie.id)` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `report` (
@@ -130,8 +131,8 @@ CREATE TABLE `report` (
   `is_deleted` bit(1) NOT NULL DEFAULT b'0',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `report.user_id,user.id` (`user_id`),
-  CONSTRAINT `report.user_id,user.id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  KEY `FOREIGN KEY(report.user_id,user.id)` (`user_id`) USING BTREE,
+  CONSTRAINT `FOREIGN KEY(report.user_id,user.id)` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `user` (
@@ -148,13 +149,13 @@ CREATE TABLE `user` (
   `is_deleted` bit(1) NOT NULL DEFAULT b'0',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `handle` (`handle`),
-  UNIQUE KEY `profile_image_id` (`profile_media_id`),
-  UNIQUE KEY `banner_image_id` (`banner_media_id`),
-  UNIQUE KEY `verification_key` (`verification_key`),
-  CONSTRAINT `user.banner_media_id,media.id` FOREIGN KEY (`banner_media_id`) REFERENCES `media` (`id`),
-  CONSTRAINT `user.profile_media_id,media.id` FOREIGN KEY (`profile_media_id`) REFERENCES `media` (`id`)
+  UNIQUE KEY `UNIQUE INDEX(email)` (`email`) USING BTREE,
+  UNIQUE KEY `UNIQUE INDEX(handle)` (`handle`) USING BTREE,
+  UNIQUE KEY `UNIQUE INDEX(profile_media_id)` (`profile_media_id`) USING BTREE,
+  UNIQUE KEY `UNIQUE INDEX(banner_media_id)` (`banner_media_id`) USING BTREE,
+  UNIQUE KEY `UNIQUE INDEX(verification_key)` (`verification_key`) USING BTREE,
+  CONSTRAINT `FOREIGN KEY(user.banner_media_id,media.id)` FOREIGN KEY (`banner_media_id`) REFERENCES `media` (`id`),
+  CONSTRAINT `FOREIGN KEY(user.profile_media_id,media.id)` FOREIGN KEY (`profile_media_id`) REFERENCES `media` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `user_history` (
@@ -164,13 +165,13 @@ CREATE TABLE `user_history` (
   `is_deleted` bit(1) NOT NULL DEFAULT b'0',
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `user_history.user_id,user.id` (`user_id`),
-  KEY `user_history.movie_id,movie.id` (`movie_id`),
-  CONSTRAINT `user_history.movie_id,movie.id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
-  CONSTRAINT `user_history.user_id,user.id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  KEY `FOREIGN KEY(user_history.user_id,user.id)` (`user_id`) USING BTREE,
+  KEY `FOREIGN KEY(user_history.movie_id,movie.id)` (`movie_id`) USING BTREE,
+  CONSTRAINT `FOREIGN KEY(user_history.movie_id,movie.id)` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
+  CONSTRAINT `FOREIGN KEY(user_history.user_id,user.id)` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE ALGORITHM=TEMPTABLE SQL SECURITY DEFINER VIEW `current_movie_statistic` AS select `isegye`.`movie_statistic`.`id` AS `id`,`isegye`.`movie_statistic`.`movie_id` AS `movie_id`,`isegye`.`movie_statistic`.`view_count` AS `view_count`,count(`isegye`.`movie_like`.`id`) AS `like_count`,count(`isegye`.`movie_comment`.`id`) AS `comment_count`,coalesce(avg(`isegye`.`movie_star`.`value`),0) AS `star_average`,`isegye`.`movie_statistic`.`created_at` AS `created_at` from ((((`isegye`.`movie_statistic` left join `isegye`.`movie_like` on(`isegye`.`movie_statistic`.`movie_id` = `isegye`.`movie_like`.`movie_id`)) left join `isegye`.`movie_comment` on(`isegye`.`movie_statistic`.`movie_id` = `isegye`.`movie_comment`.`movie_id`)) left join `isegye`.`movie_star` on(`isegye`.`movie_statistic`.`movie_id` = `isegye`.`movie_star`.`movie_id`)) join (select max(`isegye`.`movie_statistic`.`id`) AS `id` from `isegye`.`movie_statistic` group by `isegye`.`movie_statistic`.`movie_id`) `_movie_statistic` on(`isegye`.`movie_statistic`.`id` = `_movie_statistic`.`id`)) group by `isegye`.`movie_statistic`.`movie_id`;
+CREATE ALGORITHM=TEMPTABLE SQL SECURITY DEFINER VIEW `current_movie_statistic` AS SELECT `isegye`.`movie_statistic`.`id` AS `id`,`isegye`.`movie_statistic`.`movie_id` AS `movie_id`,`isegye`.`movie_statistic`.`view_count` AS `view_count`, COUNT(`isegye`.`movie_like`.`id`) AS `like_count`, COUNT(`isegye`.`movie_comment`.`id`) AS `comment_count`, COALESCE(AVG(`isegye`.`movie_star`.`value`), 0) AS `star_average`,`isegye`.`movie_statistic`.`created_at` AS `created_at` FROM ((((`isegye`.`movie_statistic` LEFT JOIN `isegye`.`movie_like` ON (`isegye`.`movie_statistic`.`movie_id` = `isegye`.`movie_like`.`movie_id`)) LEFT JOIN `isegye`.`movie_comment` ON (`isegye`.`movie_statistic`.`movie_id` = `isegye`.`movie_comment`.`movie_id`)) LEFT JOIN `isegye`.`movie_star` ON (`isegye`.`movie_statistic`.`movie_id` = `isegye`.`movie_star`.`movie_id`)) JOIN (SELECT MAX(`isegye`.`movie_statistic`.`id`) AS `id` from `isegye`.`movie_statistic` GROUP BY `isegye`.`movie_statistic`.`movie_id`) `_movie_statistic` ON (`isegye`.`movie_statistic`.`id` = `_movie_statistic`.`id`)) GROUP BY `isegye`.`movie_statistic`.`movie_id`;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
