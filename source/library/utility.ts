eebdd026c442c5585342f5f9e2a1328467969d57
H2Owater425 <h2o@h2owr.xyz>
2023-09-19 17:42:18 +0900

    feat: Add getMailContent function

--- source/library/utility.ts
+++ source/library/utility.ts
@@ -223,6 +223,10 @@ export function getGreatestCommonDivisor(a: number, b: number): number {
 	}
 }
 
+export function getMailContent(name: string, token: string): string {
+	return '<main style="margin:0px auto;width:540px;height:600px;border-top:#5d63bd solid 8px;padding:0 8px;display:flex;flex-direction:column;justify-content:space-around;"><header style="font-size:24px;font-weight:normal;"><h1 style="font-size:32px;">이세계</h1><h2 style="font-size:24px;font-weight:normal;">이메일 인증 안내</h2></header><section style="display:flex;flex-direction:column;justify-content:space-around;gap:50px;"><article style="height:100px;display:flex;flex-direction:column;justify-content:space-evenly;"><p>안녕하세요 <strong>' + name + '</strong>님,</p><p>이세계에 오신 것을 환영합니다.</p><p>아래 버튼을 눌러 인증을 완료해주세요.</p></article><a id="button" href="https://api.isegye.kr/auth/email?token=' + token + '"><button style="border:0;background-color:#5d63bd;color:#ffffff;width:210px;height:45px;font-size:16px;">메일 인증</button></a></section><footer style="padding-top: 16px;border-top:black solid 1px;display:flex;flex-direction:column;"><small>만약 버튼이 정상적으로 눌러지지 않는다면, 아래 링크로 접속해주세요.</small><small><a href="https://api.isegye.kr/auth/email?token=' + token + '">https://api.isegye.kr/auth/email?token=' + token + '</a></small></footer></main>';
+}
+
 /**
  * 0~9		사용자
  * 
--- source/routes/users/postUsers.controller.ts
+++ source/routes/users/postUsers.controller.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@library/database';
 import { Conflict } from '@library/httpError';
 import sendMail from '@library/mail';
-import { getEncryptedPassword } from '@library/utility';
+import { getEncryptedPassword, getMailContent } from '@library/utility';
 import { Prisma, User, UserVerification } from '@prisma/client';
 import { createHash } from 'crypto';
 import { FastifyReply, FastifyRequest } from 'fastify';
@@ -28,7 +28,7 @@ export default function (request: FastifyRequest<{
 			if(results[1] === null) {
 				return getEncryptedPassword(request['body']['password'], request['body']['email']);
 			} else {
-				return sendMail(request['body']['email'], '이세계 이메일 인증', '<main style="margin:0px auto;width:540px;height:600px;border-top:#5d63bd solid 8px;padding:0 8px;display:flex;flex-direction:column;justify-content:space-around;"><header style="font-size:24px;font-weight:normal;"><h1 style="font-size:32px;">이세계</h1><h2 style="font-size:24px;font-weight:normal;">이메일 인증 안내</h2></header><section style="display:flex;flex-direction:column;justify-content:space-around;gap:50px;"><article style="height:100px;display:flex;flex-direction:column;justify-content:space-evenly;"><p>안녕하세요 <strong>' + request['body']['name'] + '</strong>님,</p><p>이세계에 오신 것을 환영합니다.</p><p>아래 버튼을 눌러 인증을 완료해주세요.</p></article><a id="button" href="https://api.isegye.kr/auth/email?token=' + results[1]['token'] + '"><button style="border:0;background-color:#5d63bd;color:#ffffff;width:210px;height:45px;font-size:16px;">메일 인증</button></a></section><footer style="padding-top: 16px;border-top:black solid 1px;display:flex;flex-direction:column;"><small>만약 버튼이 정상적으로 눌러지지 않는다면, 아래 링크로 접속해주세요.</small><small><a href="https://api.isegye.kr/auth/email?token=' + results[1]['token'] + '">https://api.isegye.kr/auth/email?token=' + results[1]['token'] + '</a></small></footer></main>')
+				return sendMail(request['body']['email'], '이세계 이메일 인증', getMailContent(request['body']['name'], results[1]['token']))
 				.then(function (): string {
 					throw null;
 				});
@@ -50,7 +50,7 @@ export default function (request: FastifyRequest<{
 		});
 	})
 	.then(function () {
-		return sendMail(request['body']['email'], '이세계 이메일 인증', '<main style="margin:0px auto;width:540px;height:600px;border-top:#5d63bd solid 8px;padding:0 8px;display:flex;flex-direction:column;justify-content:space-around;"><header style="font-size:24px;font-weight:normal;"><h1 style="font-size:32px;">이세계</h1><h2 style="font-size:24px;font-weight:normal;">이메일 인증 안내</h2></header><section style="display:flex;flex-direction:column;justify-content:space-around;gap:50px;"><article style="height:100px;display:flex;flex-direction:column;justify-content:space-evenly;"><p>안녕하세요 <strong>' + request['body']['name'] + '</strong>님,</p><p>이세계에 오신 것을 환영합니다.</p><p>아래 버튼을 눌러 인증을 완료해주세요.</p></article><a id="button" href="https://api.isegye.kr/auth/email?token=' + token + '"><button style="border:0;background-color:#5d63bd;color:#ffffff;width:210px;height:45px;font-size:16px;">메일 인증</button></a></section><footer style="padding-top: 16px;border-top:black solid 1px;display:flex;flex-direction:column;"><small>만약 버튼이 정상적으로 눌러지지 않는다면, 아래 링크로 접속해주세요.</small><small><a href="https://api.isegye.kr/auth/email?token=' + token + '">https://api.isegye.kr/auth/email?token=' + token + '</a></small></footer></main>');
+		return sendMail(request['body']['email'], '이세계 이메일 인증', getMailContent(request['body']['name'], token));
 	})
 	.then(function () {
 		reply.status(201).send(null);
