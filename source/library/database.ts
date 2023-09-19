5fdbd6b30e6c2c77a4b3e196be905f51fceea2e2
H2Owater425 <h2o@h2owr.xyz>
2023-09-19 11:43:44 +0900

    feat: Add getUniqueHandle and getMovieViewKeys

--- source/library/database.ts
+++ source/library/database.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { Redis } from 'ioredis';
 import { logger } from './logger';
+import { randomBytes } from 'crypto';
 
 export const prisma: PrismaClient = new PrismaClient();
 
@@ -26,3 +27,42 @@ redis.on('error', function (error: Error): void {
 
 	return;
 });
+
+const handleCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.';
+
+export function getUniqueHandle(): Promise<string> {
+	let handle: string = '';
+	
+	for(const byte of randomBytes(30)) {
+		handle += handleCharacter[byte % 65];
+	}
+
+	return prisma['user'].count({
+		where: {
+			handle: handle
+		}
+	})
+	.then(function (userCount: number): Promise<string> | string {
+		if(userCount === 0) {
+			return handle;
+		} else {
+			return getUniqueHandle();
+		}
+	})
+}
+
+export function getMovieViewKeys(keys: Set<string>, cursor: string): Promise<Set<string>> {
+	return redis.scan(cursor, 'MATCH', 'movieView:*')
+	.then(function (results: [string, string[]]): Promise<Set<string>> | Set<string> {
+		console.log(keys, results)
+		for(let i: number = 0; i < results[1]['length']; i++) {
+			keys.add(results[1][i]);
+		}
+
+		if(results[0] !== '0') {
+			return getMovieViewKeys(keys, results[0]);
+		} else {
+			return keys;
+		}
+	})
+}
