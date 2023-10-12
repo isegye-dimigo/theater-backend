8c5bad6ead5657efbefb4d5529296b03568d8115
H2Owater425 <h2o@h2owr.xyz>
2023-10-12 21:03:56 +0900

    feat: Add alternative contact

--- source/routes/.well-known/getSecurityTxt.controller.ts
+++ source/routes/.well-known/getSecurityTxt.controller.ts
@@ -1,7 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export default function (request: FastifyRequest, reply: FastifyReply): void {
-	reply.send('Contact: mailto:me@kangmin.kim\nExpires: ' + ((new Date()).getFullYear() + 1) + '-01-01T00:00:00.000Z\nPreferred-Languages: ko, en\nCanonical: https://api.isegye.kr/.well-known/security.txt');
+	reply.send('Contact: mailto:me@kangmin.kim\nContact: mailto:kkm@isegye.kr\nExpires: ' + ((new Date()).getFullYear() + 1) + '-01-01T00:00:00.000Z\nPreferred-Languages: ko, en\nCanonical: https://api.isegye.kr/.well-known/security.txt');
 
 	return;
 }
