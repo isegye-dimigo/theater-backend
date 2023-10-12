a1ac9bf91f6fd65d9a433598499d56f7470a2ab4
H2Owater425 <h2o@h2owr.xyz>
2023-10-12 16:00:23 +0900

    fix: Use SSL/TLS for encryption

--- source/library/mail.ts
+++ source/library/mail.ts
@@ -4,7 +4,8 @@ import { SMTPChannel } from 'smtp-channel';
 const smtp: SMTPChannel = new SMTPChannel({
 	host: process['env']['EMAIL_HOST'],
 	port: Number.parseInt(process['env']['EMAIL_PORT']),
-	//secure: true
+	// SSL/TLS instead of STARTTLS
+	secure: true
 });
 
 export default function sendMail(email: string, title: string, content: string): Promise<boolean> {
@@ -12,12 +13,13 @@ export default function sendMail(email: string, title: string, content: string):
 	.then(function (): Promise<void> {
 		return smtp.write('EHLO server\r\n');
 	})
-	.then(function (): Promise<void> {
-		return smtp.write('STARTTLS\r\n');
-	})
-	.then(function (): Promise<void> {
-		return smtp.negotiateTLS();
-	})
+	// SSL/TLS instead of STARTTLS
+	//.then(function (): Promise<void> {
+	//	return smtp.write('STARTTLS\r\n');
+	//})
+	//.then(function (): Promise<void> {
+	//	return smtp.negotiateTLS();
+	//})
 	.then(function (): Promise<void> {
 		return smtp.write('AUTH LOGIN\r\n');
 	})
@@ -28,7 +30,7 @@ export default function sendMail(email: string, title: string, content: string):
 		return smtp.write('DATA\r\n');
 	})
 	.then(function (): Promise<void> {
-		return smtp.write('From: =?UTF-8?B?7J207IS46rOE?= <' + process['env']['EMAIL_USER'] + '>\r\nTo: <' + email + '>\r\nSubject: =?UTF-8?B?' + Buffer.from(title.replace(/\./m, '..'), 'utf-8').toString('base64') + '?=\r\nContent-Type: text/html; charset="UTF-8";\r\n\r\n' + content.replace(/[^\r]\n/g, '\r\n').replace(/^\./m, '..') + '\r\n.\r\nQUIT\r\n');
+		return smtp.write('From: =?UTF-8?B?7J207IS46rOE?= <' + process['env']['EMAIL_USER'] + '>\r\nTo: <' + email + '>\r\nSubject: =?UTF-8?B?' + Buffer.from(title.replace(/\./m, '..'), 'utf-8').toString('base64') + '?=\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Transfer-Encoding: base64\r\n\r\n' + Buffer.from(content.replace(/[^\r]\n/g, '\r\n').replace(/^\./m, '..')).toString('base64') + '\r\n.\r\nQUIT\r\n');
 	})
 	.then(function (): Promise<void> {
 		return smtp.close();
