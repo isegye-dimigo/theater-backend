a3cc331e3d5f44a5b3f95eecb7be2445677ebb1c
H2Owater425 <h2o@h2owr.xyz>
2023-10-13 15:45:49 +0900

    feat: Fix environment variable name

--- source/routes/medias/getMedias.controller.ts
+++ source/routes/medias/getMedias.controller.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 
 export default function (request: FastifyRequest, reply: FastifyReply): void {
 	reply.send({
-		baseUrl: process['env']['AWS_CLOUDFRONT_URL']
+		baseUrl: process['env']['MEDIA_SERVER_URL']
 	});
 
 	return;
