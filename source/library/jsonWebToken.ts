7efe3ffaa11dcb340de8205ba6b06ebdff2d1c96
H2Owater425 <h2o@h2owr.xyz>
2023-10-15 03:37:07 +0900

    fix: Fix types

--- source/library/jsonWebToken.ts
+++ source/library/jsonWebToken.ts
@@ -3,8 +3,8 @@ import { createHmac } from 'crypto';
 
 export default class JsonWebToken {
 	private token: string;
-	private _secretKey: string;
-	private _payload: Record<string, any>;
+	public secretKey: string;
+	public payload: unknown;
 
 	public static create(payload: Record<string, any>, secretKey: string): string {
 		const headerAndPayload: string = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.' + Buffer.from(JSON.stringify(payload)).toString('base64url');
@@ -12,50 +12,48 @@ export default class JsonWebToken {
 		return headerAndPayload + '.' + createHmac('sha512', secretKey).update(headerAndPayload).digest('base64url');
 	}
 
+	public static deepFreeze(target: unknown): void {
+		Object.freeze(target);
+
+		if(typeof(target) === 'object' && target !== null) {
+			const keys: string[] = Object.getOwnPropertyNames(target);
+
+			for(let i: number = 0; i < keys['length']; i++) {
+				// @ts-expect-error :: Already checked
+				if(typeof(target[keys[i]]) === 'object') {
+					// @ts-expect-error :: Already checked
+					this.deepFreeze(target[keys[i]]);
+				}
+			}
+		} else {
+			throw new Error('Target must be valid');
+		}
+
+		return;
+	}
+
 	constructor(token: string, secretKey: string) {
 		this['token'] = token;
 
 		try {
-			this['_payload'] = JSON.parse(Buffer.from(token.split('.')[1], 'base64url').toString('utf-8'));
-
-			this.deepFreeze(this['_payload']);
+			const tokenWithoutHeader: string = token.slice(token.indexOf('.') + 1);
+			this['payload'] = JSON.parse(Buffer.from(tokenWithoutHeader.slice(0, tokenWithoutHeader.indexOf('.')), 'base64url').toString('utf-8'));
+			
+			JsonWebToken.deepFreeze(this['payload']);
 		} catch {
 			throw new Error('Payload must be valid');
 		}
 
-		this['_secretKey'] = secretKey;
+		this['secretKey'] = secretKey;
 
 		return;
-	}
-
-	private deepFreeze(object: JsonWebToken['_payload']): void {
-		Object.freeze(object);
-
-		if(object !== null) {
-			const keys: string[] = Object.getOwnPropertyNames(object);
-
-			for(let i: number = 0; i < keys['length']; i++) {
-				if(typeof(object[keys[i]]) === 'object') {
-					this.deepFreeze(object[keys[i]]);
-				}
-			}
-		}
-
-		return;
-	}
-
-	public get payload(): JsonWebToken['_payload'] {
-		return this['_payload'];
-	}
-
-	public set secretKey(secretKey: string) {
-		this['_secretKey'] = secretKey;
 	}
 
 	public isValid(): boolean {
 		const splitTokens: string[] = this['token'].split('.');
-		const expireAt: number = this['_payload']['exp'] || Infinity;
+		// @ts-expect-error :: Already checked
+		const expireAt: number = this['payload']['exp'] || Infinity;
 
-		return this['_payload'] !== null && createHmac('sha512', this['_secretKey']).update(splitTokens.slice(0, 2).join('.')).digest('base64url') === splitTokens[2] && expireAt > getEpoch();
+		return createHmac('sha512', this['secretKey']).update(splitTokens.slice(0, 2).join('.')).digest('base64url') === splitTokens[2] && expireAt > getEpoch();
 	}
 }
