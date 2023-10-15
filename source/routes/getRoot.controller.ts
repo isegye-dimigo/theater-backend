ec2d24314078acbcab8cf70ba216b555efde1932
H2Owater425 <h2o@h2owr.xyz>
2023-10-16 01:56:02 +0900

    fix: Fix typo

--- source/routes/getRoot.controller.ts
+++ source/routes/getRoot.controller.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 
 export default function (request: FastifyRequest, reply: FastifyReply): void {
 	reply.send({
-		message: 'Ji Hoon Kim wants to go to the isegye'
+		message: 'JiHoon Kim wants to go to the isegye'
 	});
 
 	return;
