73ef7fde469b00c947fe20b87165b090d873598e
H2Owater425 <h2o@h2owr.xyz>
2023-10-23 16:03:45 +0900

    feat: Rename environment variable

--- source/routes/medias/getMedias.controller.ts
+++ source/routes/medias/getMedias.controller.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 
 export default function (request: FastifyRequest, reply: FastifyReply): void {
 	reply.send({
-		baseUrl: process['env']['MEDIA_SERVER_URL']
+		baseUrl: process['env']['STORAGE_URL']
 	});
 
 	return;
